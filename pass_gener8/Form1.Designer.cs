4df796e1965751582dfb281086dc1df2d9734691
afm175 <azzam_farid175@yahoo.com>
2020-05-06 13:55:40 +0700

    fixed window

--- pass_gener8/Form1.Designer.cs
+++ pass_gener8/Form1.Designer.cs
@@ -107,6 +107,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Password Generator";
